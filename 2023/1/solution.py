add06e994280aa71ef5e9b86a06b9244aa9a1646
Michael Green <self@michaelgreen.dev>
2023-12-03 15:23:05 -0800

    graph

--- 2023/1/solution.py
+++ 2023/1/solution.py
@@ -10,55 +10,90 @@ MAPPING = {
     "nine": "9"
 }
 
+NUMBERS = {v: v for v in MAPPING.values()}
 
-def find_first_num(items, strings=()):
-    for index, item in enumerate(items):
-        try:
-            int(item)
-            return item
-        except ValueError:
-            for string in strings:
-                if not string.startswith(item):
-                    continue
-                if items[index:index+len(string)] == string:
-                    return (
-                        MAPPING.get(string, "")
-                        or MAPPING.get(string[::-1], "")
-                    )
-    raise ValueError
+REVERSED = {k[::-1]: v for k, v in MAPPING.items()}
+
+
+class Node:
+    def __init__(self, mapping, key=None):
+        self.key = key
+        self.val = ""
+        letter_map = {}
+        for key, val in mapping.items():
+            if key == "":
+                self.val = val
+            else:
+                first, rest = key[0], key[1:]
+                letter_map.setdefault(first, {})[rest] = val
+
+        if not self.val:
+            self.items = {
+                k: Node(v, k) for k, v in letter_map.items()
+            }
+
+    def traverse(self, val):
+        if self.val:
+            return self.val
+
+        if not val:
+            return ""
+
+        key, rest = val[0], val[1:]
+        node = self.items.get(key)
+
+        if node is None:
+            if self.key:
+                return ""
+            return self.traverse(rest)
+
+        traverse = node.traverse(rest)
+
+        if not traverse:
+            return self.traverse(rest)
+
+        return traverse
 
 
 def part_one():
+    graph = Node(NUMBERS)
+
     val = 0
     with open("./fixtures/data.txt") as f:
         data = f.read()
     for line in data.splitlines():
-            first = find_first_num(line)
-            last = find_first_num(line[::-1])
-            val += int(first+last)
+        first = graph.traverse(line)
+        last = graph.traverse(line[::-1])
+        val += int(first+last)
     return val
 
 
 def part_two():
+    first_graph = Node({**MAPPING, **NUMBERS})
+    last_graph = Node({**REVERSED, **NUMBERS})
+
     val = 0
     with open("./fixtures/data.txt") as f:
         data = f.read()
     for line in data.splitlines():
-        first = find_first_num(line, MAPPING.keys())
-        last = find_first_num(line[::-1], (s[::-1] for s in MAPPING.keys()))
+        first = first_graph.traverse(line)
+        last = last_graph.traverse(line[::-1])
         val += int(first+last)
     return val
 
 
 def test():
+    first_graph = Node({**MAPPING, **NUMBERS})
+    last_graph = Node({**REVERSED, **NUMBERS})
     val = 0
+
     with open("./fixtures/test.txt") as f:
         data = f.read()
+
     for line in data.splitlines():
-        first = find_first_num(line, MAPPING.keys())
-        last = find_first_num(line[::-1], (s[::-1] for s in MAPPING.keys()))
-        join = first+last
-        val += int(join)
+        first = first_graph.traverse(line)
+        last = last_graph.traverse(line[::-1])
+        val += int(first+last)
     return val
 
 
@@ -70,3 +105,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
